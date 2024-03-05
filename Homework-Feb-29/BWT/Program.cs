5e708616c16276e074d753fb5867289a7098d737
bogdan-brodyn <mashina0oshibok@gmail.com>
2024-03-05 12:34:52 +0300

    Fix the tests

--- Homework-Feb-29/BWT/Program.cs
+++ Homework-Feb-29/BWT/Program.cs
@@ -2,24 +2,39 @@
 
 public class Program
 {
-    public static bool Test()
+    private static bool RunTestCase(
+        string testCase, 
+        string correctTransformedString, 
+        int correctSourceStringPosition)
     {
-        var testCase = new List<string> {"", "A", "ABC", "AAABBC", 
-                                            "1234567890", "ABACABA"};
-        for (int i = 0; i < testCase.Count; ++i)
+        var (transformedString, sourceStringPosition) = BWT.Transform(testCase);
+        return correctTransformedString == transformedString
+                && correctSourceStringPosition == sourceStringPosition
+                && testCase == BWT.ReverseTransform(transformedString, sourceStringPosition);
+    }
+
+    private static (int, int) RunTests()
+    {
+        var testCases = new List<string> {"", "A", "ABC", "AAABBC", "1234567890", "ABACABA"};
+        var correctTransformedStrings = new List<string> 
+                                        {"", "A", "CAB", "CAAABB", "9012345678", "BCABAAA"};
+        var correctSourceStringPositions = new List<int> {0, 0, 0, 0, 1, 2};
+        for (int i = 0; i < testCases.Count; ++i)
         {
-            var (transformedString, sourceStringPosition) = BWT.Transform(testCase[i]);
-            if (testCase[i] != BWT.ReverseTransform(transformedString, sourceStringPosition))
+            var isTestCasePassed = RunTestCase(testCases[i], 
+                                                correctTransformedStrings[i], 
+                                                correctSourceStringPositions[i]);
+            if (!isTestCasePassed)
             {
-                return false;
+                return (i, testCases.Count);
             }
         }
-        return true;
+        return (testCases.Count, testCases.Count);
     }
 
     public static void Main(string[] args)
     {
-        Console.WriteLine(Test() ? 
-                "Program has passed all the tests" : "Program failed test");
+        var (passedTestsCount, testsCount) = RunTests();
+        Console.WriteLine($"Program has passed {passedTestsCount}/{testsCount} tests");
     }
 }
