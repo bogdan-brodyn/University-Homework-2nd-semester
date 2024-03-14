75f814d7a0fea2621c30778327ca8ff6794dbd6c
bogdan-brodyn <mashina0oshibok@gmail.com>
2024-03-14 22:43:31 +0300

    Add trie implementation

---
+++ Homework-Mar-7/Trie/Trie.Tests/TestTrie.cs
@@ -0,0 +1,94 @@
+namespace Trie.Tests;
+
+/// <summary>
+/// Tests for Trie data structure.
+/// </summary>
+public class TestTrie
+{
+    /// <summary>
+    /// Checks if Add and Contains methods work correctly.
+    /// </summary>
+    [Test]
+    public void CheckAddAndContainsMethods()
+    {
+        var trie = new Trie();
+        Assert.Multiple(() =>
+        {
+            // Add the first element
+            Assert.That(trie.Add("a"), Is.True);
+            Assert.That(trie.Contains("a"), Is.True);
+            Assert.That(trie.Add("a"), Is.False);
+            Assert.That(trie.Contains("b"), Is.False);
+            Assert.That(trie.Contains("aa"), Is.False);
+
+            // Add the second element and check the first
+            Assert.That(trie.Add("b"), Is.True);
+            Assert.That(trie.Contains("a"), Is.True);
+            Assert.That(trie.Contains("b"), Is.True);
+            Assert.That(trie.Contains("aa"), Is.False);
+
+            // Add more elements and check contain method
+            Assert.That(trie.Add("aaba"), Is.True);
+            Assert.That(trie.Add("aabb"), Is.True);
+            Assert.That(trie.Add("abaa"), Is.True);
+            Assert.That(trie.Contains("aaba"), Is.True);
+            Assert.That(trie.Contains("aabb"), Is.True);
+            Assert.That(trie.Contains("abaa"), Is.True);
+            Assert.That(trie.Contains("aa"), Is.False);
+        });
+    }
+
+    /// <summary>
+    /// Checks if Remove method works correctly.
+    /// </summary>
+    [Test]
+    public void CheckRemoveMethod()
+    {
+        var trie = new Trie();
+        Assert.Multiple(() =>
+        {
+            // Add start elements
+            Assert.That(trie.Add("a"), Is.True);
+            Assert.That(trie.Add("aaba"), Is.True);
+            Assert.That(trie.Add("aabb"), Is.True);
+            Assert.That(trie.Add("abaa"), Is.True);
+
+            // Check remove method with the first element
+            Assert.That(trie.Remove("a"), Is.True);
+            Assert.That(trie.Contains("a"), Is.False);
+            Assert.That(trie.Remove("a"), Is.False);
+            Assert.That(trie.Contains("aaba"), Is.True);
+            Assert.That(trie.Contains("aabb"), Is.True);
+            Assert.That(trie.Contains("abaa"), Is.True);
+
+            // Check remove method with the second element
+            Assert.That(trie.Remove("aaba"), Is.True);
+            Assert.That(trie.Contains("aaba"), Is.False);
+            Assert.That(trie.Remove("aaba"), Is.False);
+            Assert.That(trie.Remove("a"), Is.False);
+            Assert.That(trie.Contains("aabb"), Is.True);
+            Assert.That(trie.Contains("abaa"), Is.True);
+        });
+    }
+
+    /// <summary>
+    /// Checks if HowManyStartsWithPrefix method works correctly.
+    /// </summary>
+    [Test]
+    public void CheckHowManyStartsWithPrefixMethod()
+    {
+        var trie = new Trie();
+        Assert.Multiple(() =>
+        {
+            Assert.That(trie.HowManyStartsWithPrefix("a"), Is.EqualTo(0));
+            Assert.That(trie.Add("a"), Is.True);
+            Assert.That(trie.HowManyStartsWithPrefix("a"), Is.EqualTo(1));
+            Assert.That(trie.Add("aaba"), Is.True);
+            Assert.That(trie.Add("aabb"), Is.True);
+            Assert.That(trie.Add("abaa"), Is.True);
+            Assert.That(trie.HowManyStartsWithPrefix("a"), Is.EqualTo(4));
+            Assert.That(trie.Remove("aabb"), Is.True);
+            Assert.That(trie.HowManyStartsWithPrefix("a"), Is.EqualTo(3));
+        });
+    }
+}
---
+++ Homework-Mar-7/Trie/Trie/Trie.cs
@@ -0,0 +1,129 @@
+﻿namespace Trie;
+
+/// <summary>
+/// Trie data structure implementation.
+/// </summary>
+public class Trie
+{
+    private readonly Vertex root = new ();
+
+    /// <summary>
+    /// Gets elements count.
+    /// </summary>
+    public int Size { get; private set; } = 0;
+
+    /// <summary>
+    /// Check if the element is contained.
+    /// </summary>
+    /// <param name="element">The element to check.</param>
+    /// <returns>True if the element is contained.</returns>
+    public bool Contains(string element)
+    {
+        var currentVertex = this.root;
+        foreach (var currentChar in element)
+        {
+            var isContained = currentVertex.Next.TryGetValue(currentChar, out var nextVertex);
+            if (!isContained || nextVertex is null)
+            {
+                return false;
+            }
+
+            currentVertex = nextVertex;
+        }
+
+        return currentVertex.IsTerminal;
+    }
+
+    /// <summary>
+    /// Add the element to data structure.
+    /// </summary>
+    /// <param name="element">Element to add.</param>
+    /// <returns>True if the element was not previously contained.</returns>
+    public bool Add(string element)
+    {
+        if (this.Contains(element))
+        {
+            return false;
+        }
+
+        this.Size++;
+        var currentVertex = this.root;
+        foreach (var currentChar in element)
+        {
+            var isContained = currentVertex.Next.TryGetValue(currentChar, out var nextVertex);
+            if (!isContained || nextVertex is null)
+            {
+                nextVertex = new Vertex();
+                currentVertex.Next.Add(currentChar, nextVertex);
+            }
+
+            currentVertex = nextVertex;
+            currentVertex.Count++;
+        }
+
+        currentVertex.IsTerminal = true;
+        return true;
+    }
+
+    /// <summary>
+    /// Get count of elements starting with the prefix.
+    /// </summary>
+    /// <param name="prefix">The prefix.</param>
+    /// <returns>Count of elements starting with the prefix.</returns>
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        var currentVertex = this.root;
+        foreach (var currentChar in prefix)
+        {
+            var isContained = currentVertex.Next.TryGetValue(currentChar, out var nextVertex);
+            if (!isContained || nextVertex is null)
+            {
+                return 0;
+            }
+
+            currentVertex = nextVertex;
+        }
+
+        return currentVertex.Count;
+    }
+
+    /// <summary>
+    /// Remove the element from data structure.
+    /// </summary>
+    /// <param name="element">The element to remove.</param>
+    /// <returns>True if the element was contained.</returns>
+    public bool Remove(string element)
+    {
+        if (!this.Contains(element))
+        {
+            return false;
+        }
+
+        this.Size--;
+        var currentVertex = this.root;
+        foreach (var currentChar in element)
+        {
+            var nextVertex = currentVertex.Next[currentChar];
+            if (nextVertex.Count == 1)
+            {
+                currentVertex.Next.Remove(currentChar);
+                return true;
+            }
+
+            currentVertex = nextVertex;
+            currentVertex.Count--;
+        }
+
+        currentVertex.IsTerminal = false;
+        return true;
+    }
+
+    private class Vertex
+    {
+        public Dictionary<char, Vertex> Next { get; } = new ();
+
+        public bool IsTerminal { get; set; } = false;
+
+        public int Count { get; set; } = 0;
+    }
+}
