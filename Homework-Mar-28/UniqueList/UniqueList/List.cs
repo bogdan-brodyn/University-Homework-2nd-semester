5ad3787236514a251263975126ecc334b020729f
bogdan-brodyn <mashina0oshibok@gmail.com>
2024-03-26 00:22:01 +0300

    Add List and UniqueList implementations

---
+++ Homework-Mar-28/UniqueList/UniqueList/List.cs
@@ -0,0 +1,78 @@
+namespace UniqueList;
+
+using System.Drawing;
+
+/// <summary>
+/// List data structure implementation.
+/// </summary>
+/// <typeparam name="TValue">Type of value the list contains.</typeparam>
+public class List<TValue>
+{
+    /// <summary>
+    /// TValue[] link.
+    /// </summary>
+#pragma warning disable SA1401 // Fields should be private
+    protected TValue[] array = new TValue[1];
+#pragma warning restore SA1401 // Fields should be private
+
+    /// <summary>
+    /// Gets or sets count of elements the list contains.
+    /// </summary>
+    public int Size { get; protected set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the maximum count of elements the list can contain before resizing.
+    /// </summary>
+    public int Capasity { get; protected set; } = 1;
+
+    /// <summary>
+    /// Add a new element to the list end.
+    /// </summary>
+    /// <param name="value">New element's value.</param>
+    public virtual void Add(TValue value)
+    {
+        if (Size >= Capasity)
+        {
+            Array.Resize(ref array, Capasity *= 2);
+        }
+
+        array[Size++] = value;
+    }
+
+    /// <summary>
+    /// Remove an element at the list end.
+    /// </summary>
+    public void Remove() => --Size;
+
+    /// <summary>
+    /// Modify the value of an element the list contains.
+    /// </summary>
+    /// <param name="newValue">The new value of an element.</param>
+    /// <param name="position">Element's position.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Position argument is out of range.</exception>
+    public virtual void ModifyAt(TValue newValue, int position)
+    {
+        if (position < 0 || position >= Size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(position));
+        }
+
+        array[position] = newValue;
+    }
+
+    /// <summary>
+    /// Gets the value of an element the list contains.
+    /// </summary>
+    /// <param name="position">Element's position.</param>
+    /// <returns>Element's value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Position argument is out of range.</exception>
+    public TValue GetBy(int position)
+    {
+        if (position < 0 || position >= Size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(position));
+        }
+
+        return array[position];
+    }
+}
---
+++ Homework-Mar-28/UniqueList/UniqueList/UniqueList.cs
@@ -0,0 +1,57 @@
+﻿namespace UniqueList;
+
+/// <summary>
+/// Unique list data structure implementation where elements' values must be unique.
+/// </summary>
+/// <typeparam name="TValue">Type of value the list contains (must be IEquatable).</typeparam>
+public class UniqueList<TValue> : List<TValue>
+    where TValue : IEquatable<TValue>
+{
+    /// <summary>
+    /// Add a new element to the list end.
+    /// </summary>
+    /// <param name="value">New element's value (must be unique).</param>\
+    /// <exception cref="NotUniqueValueException">The value of a new element is not unique.</exception>
+    public override void Add(TValue value)
+    {
+        for (var i = 0; i < Size; ++i)
+        {
+            if (Equals(value, GetBy(i)))
+            {
+                throw new NotUniqueValueException();
+            }
+        }
+
+        base.Add(value);
+    }
+
+    /// <summary>
+    /// Modify the value of an element the list contains.
+    /// </summary>
+    /// <param name="newValue">The new value of an element (should be unique).</param>
+    /// <param name="position">Element's position.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Position argument is out of range.</exception>
+    /// <exception cref="NotUniqueValueException">The new value of an element is not unique.</exception>
+    public override void ModifyAt(TValue newValue, int position)
+    {
+        if (position < 0 || position >= Size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(position));
+        }
+
+        if (Equals(newValue, GetBy(position)))
+        {
+            return;
+        }
+
+        for (var i = 0; i < Size; ++i)
+        {
+            if (Equals(newValue, GetBy(i)))
+            {
+                throw new NotUniqueValueException();
+            }
+        }
+
+        base.ModifyAt(newValue, position);
+    }
+}
